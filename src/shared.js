41704966ff49ea6cd3316e745cf1cbf143bfff8d
Taggart Bowen-Gaddy <taggart.bowen.gaddy@thesis.co>
2019-08-28 15:10:47 -0400

    Introduce Router
    - Introduce React Router
    - Move Main Content to its own Component
    - Break out some constants to a shared file

---
+++ src/shared.js
@@ -0,0 +1,23 @@
+const sections = {
+    HOME: 'home',
+    SLACK: 'slack',
+    GITHUB: 'github',
+    MAILING_LIST: 'mailing-list',
+    LEARN: 'learn',
+    USES: 'uses',
+    DESCRIPTION: 'description',
+    TEAM: 'team',
+    DEFINITION: 'definition',
+    ADVISORS: 'advisors',
+    PARTNERS: 'partners',
+    SUPPORTERS: 'supporters',
+    HIRING: 'hiring'
+}
+
+const WHITEPAPER_URL = 'https://keep.network/whitepaper'
+
+
+export {
+    sections,
+    WHITEPAPER_URL
+}

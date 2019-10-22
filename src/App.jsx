f836faceb38e5276a6c7064ecd6162d85f49c58f
Taggart Bowen-Gaddy <taggart.bowen.gaddy@thesis.co>
2019-10-22 09:59:09 -0400

    Show footer on all 'app' level pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
-import { Alert } from 'react-bootstrap'
+import { Alert, Grid, Col, Row } from 'react-bootstrap'
 import classNames from 'classnames'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Picture } from 'react-responsive-picture'
 
 import MainContent from './MainContent'
 import NavContent from './NavContent';
 import PressContent from './PressContent'
+import PageSection from './PageSection'
 import { routes } from './shared'
+import * as Icons from './Icons'
+import { sections, WHITEPAPER_URL } from './shared'
+import { getSrc } from './utils'
 
 import './app.css'
 
@@ -24,6 +29,45 @@ class App extends Component {
                     <NavContent />
                     <Route path={routes.MAIN} exact component={() => <MainContent {...this.props} />} />
                     <Route path={routes.PRESS} component={PressContent} />
+                    <PageSection id={sections.HIRING} convex>
+                        <Row>
+                            <Col sm={12} md={8}>
+                                <h2>Interested in working with us?</h2>
+                                <h3>
+
+                                    email
+                                    <a className="email-text" href="mailto:work@keep.network">
+                                        work@keep.network
+                                    </a>
+                                    <a className="btn btn-primary" href="mailto:work@keep.network">
+                                        <Icons.ArrowRight/>
+                                    </a>
+                                </h3>
+                            </Col>
+                        </Row>
+                    </PageSection>
+                    <footer>
+                        <Grid>
+                            <div className="footer-columns">
+                                <div className="footer-column-1">
+                                    <Icons.KeepCircle />
+                                </div>
+                                <ul className="footer-column-2">
+                                    <li><a href={WHITEPAPER_URL} rel="noopener noreferrer" target="_blank">Whitepaper</a></li>
+                                    <li><a href="#team">Team</a></li>
+                                    <li><a href="#advisors">Advisors</a></li>
+                                    <li><a href="https://blog.keep.network" rel="noopener noreferrer" target="_blank">Blog</a></li>
+                                </ul>
+                                <ul className="footer-column-3">
+                                    <li><a href="https://twitter.com/keep_project" rel="noopener noreferrer" target="_blank">Twitter</a></li>
+                                    <li><a href="https://t.me/KeepNetworkOfficial/" rel="noopener noreferrer" target="_blank">Telegram</a></li>
+                                    <li><a href="https://www.reddit.com/r/KeepNetwork/" rel="noopener noreferrer" target="_blank">Reddit</a></li>
+                                </ul>
+                            </div>
+                            <span>&#169; 2019 Keep. All Rights Reserved.</span>
+                            <Picture className="half-circle" src={getSrc('/images/texture-circle-3', 'png', 3)} />
+                        </Grid>
+                    </footer>
                     { alertMessage && <Alert bsStyle="info">
                             <p>{ alertMessage }</p>
                         </Alert> }
--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Col, Grid, Row } from 'react-bootstrap'
+import { Button, Col, Row } from 'react-bootstrap'
 import { Picture } from 'react-responsive-picture'
 import { scroller } from 'react-scroll'
 import { withRouter } from "react-router";
@@ -486,45 +486,6 @@ class MainContent extends Component {
                         </Col>
                     </Row>
                 </PageSection>
-                <PageSection id={sections.HIRING} convex>
-                    <Row>
-                        <Col sm={12} md={8}>
-                            <h2>Interested in working with us?</h2>
-                            <h3>
-
-                                email
-                                <a className="email-text" href="mailto:work@keep.network">
-                                    work@keep.network
-                                </a>
-                                <a className="btn btn-primary" href="mailto:work@keep.network">
-                                    <Icons.ArrowRight/>
-                                </a>
-                            </h3>
-                        </Col>
-                    </Row>
-                </PageSection>
-                <footer>
-                    <Grid>
-                        <div className="footer-columns">
-                            <div className="footer-column-1">
-                                <Icons.KeepCircle />
-                            </div>
-                            <ul className="footer-column-2">
-                                <li><a href={WHITEPAPER_URL} rel="noopener noreferrer" target="_blank">Whitepaper</a></li>
-                                <li><a href="#team">Team</a></li>
-                                <li><a href="#advisors">Advisors</a></li>
-                                <li><a href="https://blog.keep.network" rel="noopener noreferrer" target="_blank">Blog</a></li>
-                            </ul>
-                            <ul className="footer-column-3">
-                                <li><a href="https://twitter.com/keep_project" rel="noopener noreferrer" target="_blank">Twitter</a></li>
-                                <li><a href="https://t.me/KeepNetworkOfficial/" rel="noopener noreferrer" target="_blank">Telegram</a></li>
-                                <li><a href="https://www.reddit.com/r/KeepNetwork/" rel="noopener noreferrer" target="_blank">Reddit</a></li>
-                            </ul>
-                        </div>
-                        <span>&#169; 2019 Keep. All Rights Reserved.</span>
-                        <Picture className="half-circle" src={getSrc('/images/texture-circle-3', 'png', 3)} />
-                    </Grid>
-                </footer>
             </div>
         )
     }

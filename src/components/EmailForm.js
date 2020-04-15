e64f980979e9bf3f65ab3b5d61a5e5c08a0a5e46
ironng <ironng@users.noreply.github.com>
2020-04-15 16:51:27 -0700

    Remove pascal-case package

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Label, Form, FormGroup, Input } from 'reactstrap'
 import isEmail from 'validator/lib/isEmail'
-import pascalCase from 'pascal-case'
 import classNames from 'classnames'
 import merge from 'lodash/merge'
 

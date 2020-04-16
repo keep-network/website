914f0343b6fcb8419da106230db2414ac7827471
ironng <ironng@users.noreply.github.com>
2020-04-15 17:04:42 -0700

    Remove unused netlify uploadcare and cloudinary packages

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,1 @@
 import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
-
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)

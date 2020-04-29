541d843107eced62ca2c08475b51fc67caf11fe6
ironng <ironng@users.noreply.github.com>
2020-04-28 18:52:10 -0700

    Add gatsby-node.js to dynamically generate pages
    Also init CMS

---
+++ gatsby-node.js
@@ -0,0 +1,55 @@
+const path = require(`path`)
+const { createFilePath } = require("gatsby-source-filesystem")
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions
+
+  const result = await graphql(`
+    {
+      allMarkdownRemark(limit: 1000) {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+            frontmatter {
+              template
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    const templateName = node.frontmatter.template || `default`
+    const template = path.resolve(
+      path.join("src/templates/", `${templateName}.js`)
+    )
+    createPage({
+      path: node.fields.slug,
+      component: template,
+      context: { id: node.id }, // additional data can be passed via context
+    })
+  })
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}

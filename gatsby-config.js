fe43da4504eb045a69561165b1239e607e12bec3
ironng <ironng@users.noreply.github.com>
2020-05-19 16:35:22 -0700

    Create cms config for footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,6 +53,13 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
+        path: `${__dirname}/src/footer`,
+        name: "footer",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
         path: `${__dirname}/src/images`,
         name: "images",
       },

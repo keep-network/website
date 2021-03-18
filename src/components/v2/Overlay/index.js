3f4f74db022796373a712940349cdd2995fe23c3
maymax777 <dev.bugger777@yahoo.com>
2021-03-18 14:16:50 -0400

    Add container layout

--- src/components/v2/Overlay/index.js
+++ src/components/v2/Overlay/index.js
@@ -1,12 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Container } from "reactstrap"
 
 function Overlay(props) {
   const { children } = props
 
   return (
     <section>
-      <div className="page-section-content">{children}</div>
+      <div className="page-section-content">
+        <Container fluid="md"> {children} </Container>
+      </div>
     </section>
   )
 }

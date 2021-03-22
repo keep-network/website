648dc5c369d9b2fc921d0daef26d9ce26b632734
maymax777 <dev.bugger777@yahoo.com>
2021-03-22 13:24:33 -0400

    Update overlay component

--- src/components/v2/Overlay/index.js
+++ src/components/v2/Overlay/index.js
@@ -1,24 +1,73 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
+import classNames from "classnames"
 import { Container } from "reactstrap"
 
-function Overlay(props) {
-  const { children } = props
+import { ArrowRight } from "../../Icons"
+
+export const SeeAllButton = ({ collapsed = true, onClick = () => {} }) => (
+  <button className={classNames("see-all", { collapsed })} onClick={onClick}>
+    <span>{collapsed ? "See all" : "See less"}</span>
+    <ArrowRight />
+  </button>
+)
+
+SeeAllButton.propTypes = {
+  collapsed: PropTypes.bool,
+  onClick: PropTypes.func,
+}
+
+const PageSection = ({
+  id,
+  additionalClassNames,
+  children,
+  collapsible,
+  style,
+}) => {
+  const [isCollapsed, setIsCollapsed] = useState(collapsible ? true : false)
+
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed)
+  }
+
+  const classes = classNames(
+    "page-section",
+    { collapsible: collapsible, collapsed: isCollapsed },
+    id,
+    additionalClassNames
+  )
 
   return (
-    <section>
+    <section className={classes} id={id} style={style}>
       <div className="page-section-content">
-        <Container fluid="md"> {children} </Container>
+        <Container fluid="md">
+          {children}
+          {collapsible ? (
+            <SeeAllButton collapsed={isCollapsed} onClick={toggleCollapse} />
+          ) : (
+            ""
+          )}
+        </Container>
       </div>
     </section>
   )
 }
 
-Overlay.propTypes = {
+PageSection.propTypes = {
+  id: PropTypes.string,
+  additionalClassNames: PropTypes.array,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]),
+  collapsible: PropTypes.bool,
+  style: PropTypes.object,
 }
 
-export default Overlay
+PageSection.defaultProps = {
+  id: "",
+  additionalClassNames: [],
+  collapsible: false,
+}
+
+export default PageSection

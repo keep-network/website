dde5d86c9c7b28230982973b47d8d8944a930b52
Satoru Kato <dev.bugger777@yahoo.com>
2021-03-23 13:30:56 -0400

    Update button color style

--- src/components/v2/Overlay/index.js
+++ src/components/v2/Overlay/index.js
@@ -1,20 +1,25 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Section from "../Section"
+import Button from "../Button"
 
 function Overlay(props) {
   return (
     <div className="overlay">
-      <h1>
-        Stake on EStake on Ethereum’s first private computer and earn up to 200%
-        APY.
-      </h1>
-      <h4>
-        Staking with Keep is the best way to back a truly decentralized network
-        and the future of DeFi.
-      </h4>
-      <div className="button-group">
-        
-      </div>
+      <Section>
+        <div className="overlay-content">
+          <h1>
+            Stake on EStake on Ethereum’s first private computer and earn up to 200% APY.
+          </h1>
+          <h4>
+            Staking with Keep is the best way to back a truly decentralized network and the future of DeFi.
+          </h4>
+          <div className="button-group">
+            <Button label="ABOUT KEEP" type="primary" />
+            <Button label="BUTTON" type="secondary" />
+          </div>
+        </div>
+      </Section>
     </div>
   )
 }

36a0f3a80e78e383562423fe367cb96d7d067b94
maymax777 <dev.bugger777@yahoo.com>
2021-03-31 09:56:54 -0400

    Fix lint issue

--- src/components/v2/Overlay/index.js
+++ src/components/v2/Overlay/index.js
@@ -11,7 +11,8 @@ function Overlay(props) {
           Stake on Ethereum’s first private computer and earn up to 200% APY.
         </h1>
         <h4>
-          Staking with Keep is the best way to back a truly decentralized network and the future of DeFi.
+          Staking with Keep is the best way to back a truly decentralized
+          network and the future of DeFi.
         </h4>
         <div className="button-group">
           <Button label="ABOUT KEEP" type="primary" />

013d3e5d8ada5b012180d1ee937da325ffaf8095
FrozenIce0617 <prosdev087@gmail.com>
2021-04-09 18:00:38 +0200

    feat(data)/srt-21: data modeling for FAQ page

---
+++ src/pages/faq.md
@@ -0,0 +1,49 @@
+---
+template: faq-page
+path: /faq
+title: FAQ
+hero:
+  title: FAQ
+questions:
+  - question: What is the Keep network?
+    answer: Here is an answer.
+  - question: How does a keep work?
+    answer: Here is an answer.
+  - question: What is a KEEP token?
+    answer: Here is an answer.
+  - question: Is Keep live?
+    answer: Here is an answer.
+  - question: How do I get KEEP tokens?
+    answer: Here is an answer.
+  - question: How do I stake KEEP tokens?
+    answer: Here is an answer.
+  - question: Is there a minimum staking amount?
+    answer: Here is an answer.
+  - question: What is the difference between “staking” and “stakedrop”?
+    answer: Here is an answer.
+  - question: What are the risks of staking?
+    answer: Here is an answer.
+  - question: What are the rewards of staking?
+    answer: Here is an answer.
+contact:
+  title: Keep in touch
+  header: Get the Keep Newsletter
+  description: Sign up for our newsletter and never miss an update.
+  cards:
+    - title: Read the Keep blog
+      body: Get the latest updates on the blog. Read about the Keep network, tBTC, partnerships, and more.
+      icon:
+        image: /images/svg/contact-icon-1.svg
+        alt: Read the Keep blog
+      link:
+        name: Read Blog
+        url: https://www.fabric.vc/
+    - title: Join the community
+      body: Connect with the Discord community, ask questions, and get in on the ground level for the future of DeFi.
+      icon:
+        image: /images/svg/contact-icon-2.svg
+        alt: Join the community
+      link:
+        name: Join Discord
+        url: https://www.fabric.vc/
+---
---
+++ src/templates/faq-page.js
@@ -0,0 +1,130 @@
+import React from "react"
+import { connect } from "react-redux"
+import { Col, Row } from "reactstrap"
+import PropTypes from "prop-types"
+import { graphql } from "gatsby"
+
+import { App, PageSection, Image, Contact } from "../components"
+import { sections } from "../constants"
+import { actions } from "../redux"
+
+export const FaqPageTemplate = ({
+  hero = {},
+  questions = [],
+  contact = {},
+  signupMailingList = () => {},
+  ajaxRequestStates = {},
+  plusIcon = {},
+}) => {
+  return (
+    <div className="main-content">
+      <PageSection id={sections.faq.HOME}>
+        <Row>
+          <Col xs={12}>
+            <h1 dangerouslySetInnerHTML={{ __html: hero.title }} />
+          </Col>
+        </Row>
+      </PageSection>
+      <PageSection id={sections.faq.QUESTIONS}>
+        <Row className="questions-section">
+          <Col xs={12} sm={12}>
+            {questions.map((item, i) => (
+              <div key={`question-${i}`}>
+                <p>
+                  {item.question}
+                  <Image imageData={plusIcon} />
+                </p>
+              </div>
+            ))}
+          </Col>
+        </Row>
+      </PageSection>
+      <PageSection id={sections.home.CONTACT}>
+        <Row>
+          <Col xs={12} sm={12}>
+            <Contact
+              {...contact}
+              signupMailingList={signupMailingList}
+              requestStates={ajaxRequestStates}
+            />
+          </Col>
+        </Row>
+      </PageSection>
+    </div>
+  )
+}
+
+FaqPageTemplate.propTypes = {
+  hero: PropTypes.object,
+  questions: PropTypes.array,
+  contact: PropTypes.object,
+  signupMailingList: PropTypes.func,
+  ajaxRequestStates: PropTypes.object,
+  plusIcon: PropTypes.object,
+}
+
+const mapStateToProps = (state) => ({
+  ajaxRequestStates: state.ajaxRequestStates,
+})
+
+export const ConnectedFaqPage = connect(mapStateToProps, {
+  signupMailingList: actions.signupMailingList,
+})(FaqPageTemplate)
+
+const FaqPage = ({ data }) => {
+  const { markdownRemark: post, plusIcon } = data
+  console.log("icon:", plusIcon)
+  return (
+    <App className="app-home">
+      <ConnectedFaqPage {...post.frontmatter} plusIcon={{ image: plusIcon }} />
+    </App>
+  )
+}
+
+FaqPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+    plusIcon: PropTypes.object,
+  }),
+}
+
+export default FaqPage
+
+export const query = graphql`
+  query FaqPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      frontmatter {
+        hero {
+          title
+        }
+        questions {
+          question
+          answer
+        }
+        contact {
+          title
+          header
+          description
+          cards {
+            title
+            body
+            icon {
+              image {
+                relativePath
+              }
+              alt
+            }
+            link {
+              name
+              url
+            }
+          }
+        }
+      }
+    }
+    plusIcon: file(relativePath: { regex: "/ic-plus.png/" }) {
+      relativePath
+    }
+  }
+`

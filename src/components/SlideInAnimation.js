33ebbe479bcf3ee65e049c52ec5e7e4181a56251
michalsmiarowski <michal.smiarowski@thesis.co>
2021-05-31 14:12:23 +0200

    Remove unnecesary semicolon

--- src/components/SlideInAnimation.js
+++ src/components/SlideInAnimation.js
@@ -1,5 +1,5 @@
 import React from "react"
-import PropTypes from "prop-types";
+import PropTypes from "prop-types"
 
 const SlideInAnimation = ({ durationInSec = 1, children }) => {
   const chars = children.split("")

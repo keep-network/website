2aeb4f0b297ea9b4f3837a990bbc0e57e182f868
michalsmiarowski <michal.smiarowski@thesis.co>
2022-05-04 17:54:36 +0200

    Remove TBTC-v2/Saddle-V2 pool

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -43,23 +43,21 @@ export const HomePageTemplate = ({
     Aos.init({ once: true })
   }, [])
 
-  const [
-    liquidityRewardsAPYs,
-    areLiquidityRewardsAPYsFetching,
-  ] = useLiquidityRewardsAPY()
+  // const [
+  //   liquidityRewardsAPYs,
+  //   areLiquidityRewardsAPYsFetching,
+  // ] = useLiquidityRewardsAPY()
 
   const [coveragePoolAPY, isCoveragePoolAPYFetching] = useCoveragePoolsAPY()
 
   const isFetching = useMemo(() => {
-    return isCoveragePoolAPYFetching || areLiquidityRewardsAPYsFetching
-  }, [isCoveragePoolAPYFetching, areLiquidityRewardsAPYsFetching])
+    return isCoveragePoolAPYFetching
+  }, [isCoveragePoolAPYFetching])
 
   const APYs = useMemo(() => {
     if (isFetching) return []
-    return [...liquidityRewardsAPYs, ...coveragePoolAPY].sort(
-      (a, b) => b.value - a.value
-    )
-  }, [isFetching, liquidityRewardsAPYs, coveragePoolAPY])
+    return [...coveragePoolAPY].sort((a, b) => b.value - a.value)
+  }, [isFetching, coveragePoolAPY])
 
   const renderHighestAPY = () => {
     if (APYs.length === 0) {

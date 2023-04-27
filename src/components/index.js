b7fa18f67d4dc90e46b75ec0b7960fad607d4ac5
Rafal Czajkowski <rafal.czajkowski@keep.network>
2023-04-27 13:10:33 +0200

    Remove unnecessary `SummaryGrid` component

--- src/components/index.js
+++ src/components/index.js
@@ -15,7 +15,6 @@ import PageSection from "./PageSection"
 import Profile from "./Profile"
 import SubmitButton from "./SubmitButton"
 import Ticker from "./Ticker"
-import SummaryGrid from "./SummaryGrid"
 import MiniLogoWall from "./MiniLogoWall"
 import KeepBlog from "./KeepBlog"
 
@@ -37,7 +36,6 @@ export {
   Profile,
   SubmitButton,
   Ticker,
-  SummaryGrid,
   MiniLogoWall,
   KeepBlog,
 }
